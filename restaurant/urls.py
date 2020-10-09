c0131968dedf85f4705916f07d32b8f7c1bd1841
chenwei713 <chenwei713@foxmail.com>
2020-10-09 17:45:05 -0400

    1. Change get restaurant by name API. 2. Create add inspection records API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,5 +5,6 @@ from restaurant import views
 app_name = 'restaurant'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<restaurant_name>/', views.get_restaurant_by_name, name='restaurant')
+    path('<restaurant_name>/', views.get_restaurant_by_name, name='restaurant'),
+    path('add_inspection_records/', views.add_inspection_records, name='add_inspection_records')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Restaurant
-import requests
+from .utils import query_yelp
 from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+from django.core import serializers
 import json
 
 
-# Create your views here.
 def index(request):
     return HttpResponse("Hello, this is restaurant.")
 
+
+def add_inspection_records(request):
+    # TODO: check login status
+    records = json.loads(request.body)
+    for record in serializers.deserialize("json", records):
+        # TODO: Pre-process Data
+        record.save()
+
+
 def get_restaurant_by_name(request, restaurant_name):
-    restaurant = get_object_or_404(Restaurant, restaurant_name=restaurant_name)
-    business_id = restaurant.business_id
-    access_token = 'A_V_V4rxelsvDsI2uFW1kT2mP2lUjd75GTEEsEcLnnvVOK5ssemrbw-R49czpANtS2ZtAeCl6FaapQrp1_30cRt9YKao3pFL1I6304sAtwKwKJkF1JBgF88FZl1_X3Yx'
-    headers = {'Authorization': 'bearer %s' % access_token}
-    response = {}
-    response['info'] = json.loads(requests.get('https://api.yelp.com/v3/businesses/' + business_id, headers=headers).content)
-    response['reviews'] = json.loads(requests.get('https://api.yelp.com/v3/businesses/' + business_id + '/reviews', headers=headers).content)
+    # TODO: Restaurant Model primary key haven't decided
+    restaurant = Restaurant.objects.get(restaurant_name=restaurant_name)
+    if not restaurant:
+        # TODO: Query yelp with matching module
+        return HttpResponseNotFound('Restaurant not found')
+    response = query_yelp(restaurant.business_id)
     return HttpResponse(json.dumps(response))
+
 
