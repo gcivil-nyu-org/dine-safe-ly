4a37e745fe21ce0e975e383659e417a9f602a523
chenwei713 <chenwei713@foxmail.com>
2020-10-09 16:48:16 -0400

    create feature branch

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from restaurant import views
 
-app_name = 'polls'
+app_name = 'restaurant'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<restaurant_name>/', views.get_restaurant_by_name, name='restaurant')
