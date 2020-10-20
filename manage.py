89954459fdcd733b793e8c97e9c2873d80b95975
vinayaknatarajan1 <nvinayak777@gmail.com>
2020-10-20 19:14:08 -0400

    update business IDs

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.base import BaseCommand
 
 def main():
     """Run administrative tasks."""
--- saveBusinessID.py
+++ saveBusinessID.py
@@ -19,7 +19,8 @@ def match_on_yelp(restaurant_name, restaurant_location):
     country = 'US'
 
     api_key = 'JaekzvTTKsWGtQ96HUiwAXOUwRt6Ndbqzch4zc2XFnOEBxwTmwr-esm1uWo2QFvFJtXS8nY2dXx51cfAnMqVHpHRcp8N7QtP7LNVCcoxJWV_9NJrmZWSMiq-R_mEX3Yx'
-    headers = {'Authorization': 'Bearer %s' % api_key}
+    YELP_ACCESS_TOKE = 'A_V_V4rxelsvDsI2uFW1kT2mP2lUjd75GTEEsEcLnnvVOK5ssemrbw-R49czpANtS2ZtAeCl6FaapQrp1_30cRt9YKao3pFL1I6304sAtwKwKJkF1JBgF88FZl1_X3Yx'
+    headers = {'Authorization': 'Bearer %s' % YELP_ACCESS_TOKE}
     url = 'https://api.yelp.com/v3/businesses/matches'
     params = {'name': restaurant_name, 'address1': address1, 'city': city, 'state': state, 'country': country}
 
@@ -27,18 +28,20 @@ def match_on_yelp(restaurant_name, restaurant_location):
     return response.text.encode("utf8")
 
 if __name__ == '__main__':
-    r = Restaurant.objects.all()[:100]
+    r = Restaurant.objects.all()[5000:]
 
     for rest in r:
-        # response = json.loads(match_on_yelp(rest.restaurant_name, rest.business_address))
-        # if next(iter(response)) == 'error':
-        #     # print("match failed")
-        #     continue
-        # elif not response['businesses']:
-        #     continue
-        # else:
-        #     rest.business_id = response['businesses'][0]['id']
-        #     rest.save()
-        #     print("success")
-        print(rest)
-        print()
+        response = json.loads(match_on_yelp(rest.restaurant_name, rest.business_address))
+        if next(iter(response)) == 'error':
+            print("match failed")
+            # print(response)
+            continue
+        elif not response['businesses']:
+            print("not success")
+            continue
+        else:
+            rest.business_id = response['businesses'][0]['id']
+            rest.save()
+            print("success")
+        # print(rest)
+        # print()
