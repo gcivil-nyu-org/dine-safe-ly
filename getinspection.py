e3cce300aabf5147681274888bcbffe5f37a17bf
vinayaknatarajan1 <nvinayak777@gmail.com>
2020-10-20 22:43:02 -0400

    get business IDs

--- getinspection.py
+++ getinspection.py
@@ -7,11 +7,29 @@ import django
 django.setup()
 import pandas as pd
 from sodapy import Socrata
+import requests
+import json
 
 from restaurant.models import Restaurant, InspectionRecords
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
+
+def match_on_yelp(restaurant_name, restaurant_location):
+    location_list = restaurant_location.split(", ")
+    address1 = location_list[0]
+    city = location_list[1]
+    state = location_list[2]
+    country = 'US'
+
+    api_key = 'JaekzvTTKsWGtQ96HUiwAXOUwRt6Ndbqzch4zc2XFnOEBxwTmwr-esm1uWo2QFvFJtXS8nY2dXx51cfAnMqVHpHRcp8N7QtP7LNVCcoxJWV_9NJrmZWSMiq-R_mEX3Yx'
+    YELP_ACCESS_TOKE = 'A_V_V4rxelsvDsI2uFW1kT2mP2lUjd75GTEEsEcLnnvVOK5ssemrbw-R49czpANtS2ZtAeCl6FaapQrp1_30cRt9YKao3pFL1I6304sAtwKwKJkF1JBgF88FZl1_X3Yx'
+    headers = {'Authorization': 'Bearer %s' % YELP_ACCESS_TOKE}
+    url = 'https://api.yelp.com/v3/businesses/matches'
+    params = {'name': restaurant_name, 'address1': address1, 'city': city, 'state': state, 'country': country}
+
+    response = requests.get(url, params=params, headers=headers)
+    return response.text.encode("utf8")
 
 def clean_inspection_data(results_df):
     restaurant_df = results_df.loc[:,['restaurantname','legalbusinessname','businessaddress','postcode']]
@@ -25,8 +43,20 @@ def clean_inspection_data(results_df):
 def save_restaurants(restaurant_df):
     for index, row in restaurant_df.iterrows():
         try:
-            r = Restaurant(restaurant_name=row['restaurantname'],business_address=row['businessaddress'],postcode=row['postcode'],legal_business_name=row['legalbusinessname'])
-            r.save()
+            b_id = None
+            response = json.loads(match_on_yelp(row['restaurantname'], row['businessaddress']))
+            if next(iter(response)) == 'error':
+                print("match failed")
+                # print(response)
+                continue
+            elif not response['businesses']:
+                print("not success")
+                continue
+            else:
+                b_id = response['businesses'][0]['id']
+                print("success")
+                r = Restaurant(restaurant_name=row['restaurantname'],business_address=row['businessaddress'],postcode=row['postcode'],legal_business_name=row['legalbusinessname'],business_id=b_id)
+                r.save()
         except:
             continue
     return
@@ -55,6 +85,8 @@ def get_inspection_data():
     # print(lastInspection[0].inspected_on)
     # a = dateutil.parser.parse(str(lastInspection[0].inspected_on)).date()
     date = str(lastInspection[0].inspected_on)
+    print("check new date here")
+    print(date)
     date = date.replace(" ",'T')
     var = 'inspectedon > \''+ date + '\''
     # print(a)
