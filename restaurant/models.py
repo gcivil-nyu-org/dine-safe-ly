a509034b49c96d9ef1dd790f1815d421181f6ada
Alex Hong <ah4927@nyu.edu>
2020-10-22 10:12:49 -0400

    test template

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,14 +6,14 @@ class Restaurant(models.Model):
     business_address = models.CharField(max_length=200)
     postcode = models.CharField(max_length=200)
     business_id = models.CharField(max_length=200, default=None, blank=True, null=True)
-    legal_business_name = models.CharField(max_length=200, default=None)
+    # legal_business_name = models.CharField(max_length=200, default=None)
 
     class Meta:
         unique_together = (('restaurant_name', 'business_address', 'postcode'),)
 
     def __str__(self):
-        return '{} {} {} {} {} {}'.format(self.id, self.restaurant_name, self.business_address, self.postcode, self.legal_business_name, self.business_id) 
-
+        return '{} {} {} {} {} {}'.format(self.id, self.restaurant_name, self.business_address,
+                                          self.postcode, self.business_id)
 
 
 class InspectionRecords(models.Model):
@@ -24,7 +24,9 @@ class InspectionRecords(models.Model):
     is_roadway_compliant = models.CharField(max_length=200)
     skipped_reason = models.CharField(max_length=200)
     inspected_on = models.DateTimeField()
-    
+    # seating_choice = models.CharField(max_length=20)
 
     def __str__(self):
-        return '{} {} {} {} {} {} {}'.format(self.restaurant_Inspection_ID, self.restaurant_name, self.is_roadway_compliant,self.skipped_reason, self.inspected_on, self.postcode, self.business_address)
+        return '{} {} {} {} {} {} {}'.format(self.restaurant_Inspection_ID, self.restaurant_name,
+                                             self.is_roadway_compliant, self.skipped_reason, self.inspected_on,
+                                             self.postcode, self.business_address)
--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,35 @@
+import enum
 from django.test import TestCase
 
-# Create your tests here.
+from .models import InspectionRecords, Restaurant
+
+
+class Compliance(enum.Enum):
+    no = "Non-Compliant"
+    yes = "Compliant"
+    skipped = "Skipped Inspection"
+
+
+def create_restaurant(name, address, postcode, business_id):
+    return Restaurant.objects.create(restaurant_name=name, business_address=address, postcode=postcode,
+                                     business_id=business_id)
+
+
+def create_inspection_records(restaurant_inspection_id, name, postcode, business_address,
+                              compliance_status, skipped_reason, inspected_on):
+
+    if not isinstance(compliance_status, Compliance):
+        raise TypeError('compliance_status must be an instance of Compliance enum')
+
+    return InspectionRecords.objects.create(restaurant_inspection_id=restaurant_inspection_id, restaurant_name=name,
+                                            postcode=postcode, business_address=business_address,
+                                            is_roadway_compliant=compliance_status,
+                                            skipped_reason=skipped_reason, inspected_on=inspected_on)
+
+
+class InspectionRecordsViewTests(TestCase):
+    """ Test InspectionRecords Views """
+
+
+class RestaurantViewTests(TestCase):
+    """ Test Restaurant Views """
