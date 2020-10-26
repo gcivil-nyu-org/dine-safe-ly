69e11ebfba54affb03f564122c294a4cfaffcc2c
chenwei713 <chenwei713@foxmail.com>
2020-10-26 17:26:45 -0400

    update getinspection

--- getinspection.py
+++ getinspection.py
@@ -6,7 +6,7 @@ from sodapy import Socrata
 
 import requests
 import json
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 
 # import dateutil.parser
 
@@ -14,7 +14,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinesafelysite.settings")
 django.setup()
 from restaurant.models import Restaurant, InspectionRecords
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
 
 
 def match_on_yelp(restaurant_name, restaurant_location):
@@ -24,14 +24,16 @@ def match_on_yelp(restaurant_name, restaurant_location):
     state = "NY"
     country = "US"
 
-    # api_key = "JaekzvTTKsWGtQ96HUiwAXOUwRt6Ndbqzch4zc2XFnOEBxwTmwr" \
-    #           "-esm1uWo2QFvFJtXS8nY2dXx51cfAnMqVHpHRcp8N7QtP7LNVCcoxJWV_9NJrmZWSMiq" \
-    #           "-R_mEX3Yx "
     YELP_ACCESS_TOKE = (
-        "A_V_V4rxelsvDsI2uFW1kT2mP2lUjd75GTEEsEcLnnvVOK5ssemrbw"
-        "-R49czpANtS2ZtAeCl6FaapQrp1_30cRt9YKao3pFL1I6304sAtwKwKJk"
-        "F1JBgF88FZl1_X3Yx "
+        "JaekzvTTKsWGtQ96HUiwAXOUwRt6Ndbqzch4zc2XFnOEBxwTmwr" \
+        "-esm1uWo2QFvFJtXS8nY2dXx51cfAnMqVHpHRcp8N7QtP7LNVCcoxJWV_9NJrmZWSMiq" \
+        "-R_mEX3Yx "
     )
+    # YELP_ACCESS_TOKE = (
+    #     "A_V_V4rxelsvDsI2uFW1kT2mP2lUjd75GTEEsEcLnnvVOK5ssemrbw"
+    #     "-R49czpANtS2ZtAeCl6FaapQrp1_30cRt9YKao3pFL1I6304sAtwKwKJk"
+    #     "F1JBgF88FZl1_X3Yx "
+    # )
     headers = {"Authorization": "Bearer %s" % YELP_ACCESS_TOKE}
     url = "https://api.yelp.com/v3/businesses/matches"
     params = {
@@ -63,10 +65,13 @@ def clean_inspection_data(results_df):
                         "postcode",
                     ],
                     ]
-
     restaurant_df = restaurant_df.apply(
         lambda x: x.str.strip() if x.dtype == "str" else x
     )
+
+    restaurant_df = restaurant_df[restaurant_df['restaurantname'] != 'Test']
+    restaurant_df = restaurant_df[restaurant_df['restaurantname'] != 'test']
+    restaurant_df = restaurant_df[restaurant_df['restaurantname'] != 'TEST']
 
     restaurant_df.drop_duplicates(
         subset=["restaurantname", "businessaddress", "postcode"],
@@ -89,14 +94,14 @@ def save_restaurants(restaurant_df):
             else:
                 b_id = response["businesses"][0]["id"]
 
-            r = Restaurant(
-                restaurant_name=row["restaurantname"],
-                business_address=row["businessaddress"],
-                postcode=row["postcode"],
-                # legal_business_name=row["legalbusinessname"],
-                business_id=b_id,
-            )
-            r.save()
+            # r = Restaurant(
+            #     restaurant_name=row["restaurantname"],
+            #     business_address=row["businessaddress"],
+            #     postcode=row["postcode"],
+            #     # legal_business_name=row["legalbusinessname"],
+            #     # business_id=b_id,
+            # )
+            # r.save()
         except OSError:
             print("save_restaurant failed")
             raise
@@ -140,13 +145,17 @@ def get_inspection_data():
     )
 
     # results = client.get("4dx7-axux", where=date_query)
-    results = client.get("4dx7-axux")
+    results = client.get("4dx7-axux", limit=30000)
 
     # Convert to pandas DataFrame
+
     results_df = pd.DataFrame.from_records(results)
+    # results_df = pd.read_csv('/Users/xiechenwei/Downloads/opendata_restaurant.csv')
+    print(results_df.shape)
+    print(results_df.columns)
+
     if results_df.shape[0] > 0:
         restaurant_df, inspection_df = clean_inspection_data(results_df)
-        # print(restaurant_df)
         save_restaurants(restaurant_df)
         # print(inspection_df)
         save_inspections(inspection_df)
@@ -154,5 +163,31 @@ def get_inspection_data():
 
 # sched.start()
 
+def populate_restaurant_with_yelp_id():
+    restaurants = Restaurant.objects.all()[4316:6849]
+    limit = 3000
+    count = 0
+    for r in restaurants:
+        if r.business_id:
+            count += 1
+            continue
+
+        response = json.loads(match_on_yelp(r.restaurant_name, r.business_address))
+
+        if next(iter(response)) == "error":
+            continue
+        elif not response["businesses"]:
+            continue
+        else:
+            b_id = response["businesses"][0]["id"]
+        r.business_id = b_id
+        r.save()
+        count += 1
+        limit -= 1
+        if limit == 0:
+            break
+    print(count)
+
 if __name__ == '__main__':
-    get_inspection_data()
+    populate_restaurant_with_yelp_id()
+    #get_inspection_data()
