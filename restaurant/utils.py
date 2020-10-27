bbe4ea208e2939e29ceb8d5a6728de4a5ef11672
chenwei713 <chenwei713@foxmail.com>
2020-10-27 17:12:12 -0400

    Fix the latest inspection record bug in restaurant page

--- restaurant/utils.py
+++ restaurant/utils.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.forms.models import model_to_dict
 from .models import InspectionRecords, Restaurant
 import requests
+import datetime
 import json
 
 
@@ -43,7 +44,9 @@ def get_latest_inspection_record(business_name, business_address, postcode):
         postcode=postcode,
     ).order_by("-inspected_on")
     if len(records) >= 1:
-        return model_to_dict(records[0])
+        record = model_to_dict(records[0])
+        record['inspected_on'] = record['inspected_on'].strftime("%Y-%m-%d %H:%M:%S")
+        return record
 
     return None
 
