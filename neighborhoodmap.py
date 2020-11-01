ed83ab2725ae19f3e68a2d88e0228dc26b331f5a
vinayaknatarajan1 <nvinayak777@gmail.com>
2020-11-01 04:29:38 -0500

    Added script to query yelp and save restaurant details with neigbourhood mapping and restaurant category.

--- neighborhoodmap.py
+++ neighborhoodmap.py
@@ -7,7 +7,9 @@ import logging
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinesafelysite.settings")
 django.setup()
 
-from restaurant.models import Zipcodes
+from django.conf import settings
+from restaurant.models import Zipcodes, YelpRestaurantDetails, Restaurant
+from restaurant.utils import *
 
 logger = logging.getLogger(__name__)
 
@@ -25,5 +27,47 @@ def map_zipcode_to_neighbourhood():
         except Exception as e:
             logger.warning("Error while saving zipcode/neighbourhood to table Zipcodes: {}".format(zip['zip']))
 
+
+def get_neighbourhood(zip):
+    area = Zipcodes.objects.get(zipcode=zip)
+    return area.neighborhood
+
+def get_restaurant_category_yelp(alias):
+    access_token = settings.YELP_ACESS_TOKEN_BETA
+    headers = {"Authorization": "bearer %s" % access_token}
+    url = settings.YELP_CATEGORY_API + alias
+    response = requests.get(url, headers=headers)
+    return json.loads(response.content)
+
+def get_cuisine(categories):
+
+    for c in categories:
+        category = get_restaurant_category_yelp(c['alias'])
+        if category['category']['parent_aliases'][0] == "restaurants":
+            return c['alias']
+        else:
+            return category['category']['parent_aliases'][0]
+        # break
+
+def save_yelp_restaurant_details():
+
+    for r in Restaurant.objects.all():
+        try:
+            if r.business_id:
+                restaurant_info = query_yelp(r.business_id)
+                area = get_neighbourhood(restaurant_info['info']['location']['zip_code'])
+                cuisine = get_cuisine(restaurant_info['info']['categories'])
+
+                details = YelpRestaurantDetails(business_id=r.business_id, neighborhood = area,category = cuisine, price = restaurant_info['info']['price'], rating = restaurant_info['info']['rating'])
+                details.save()
+
+                logger.info("Yelp restaurant details successfully saved: {}".format(r.business_id))
+            else:
+                continue
+        except Exception as e:
+            logger.warning("Error while saving to table YelpRestaurantDetails: {}".format(r.business_id))
+
+
 if __name__ == "__main__":
-    map_zipcode_to_neighbourhood()
+    # map_zipcode_to_neighbourhood()
+    save_yelp_restaurant_details()
