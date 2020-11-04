74463f0a3a041517a426163cbc49e9b40a8120e8
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2020-11-04 00:38:19 -0500

    import-export lib

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import InspectionRecords, Restaurant
+from .models import InspectionRecords, Restaurant, UserQuestionnaire, YelpRestaurantDetails, Zipcodes
+from import_export import resources
+# from import_export.fields import Field
 
 admin.site.register(Restaurant)
 admin.site.register(InspectionRecords)
+admin.site.register(UserQuestionnaire)
+admin.site.register(YelpRestaurantDetails)
+admin.site.register(Zipcodes)
+
+
+class RestaurantResource(resources.ModelResource):
+
+    class Meta:
+        model = Restaurant
+        fields = ("id", "restaurant_name", "business_address", "postcode", "business_id")
+        export_order = ("id", "restaurant_name", "business_address", "postcode", "business_id")
