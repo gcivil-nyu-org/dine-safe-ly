ea293e82a5144ae5a6ddfeb4ac7e394a2e357faa
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2020-11-09 19:01:26 -0500

    modified customized user model'

---
+++ user/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from restaurant.models import Restaurant
+
+
+class DineSafelyUser(AbstractUser):
+
+    favorite_restaurants = models.ManyToManyField(Restaurant)
