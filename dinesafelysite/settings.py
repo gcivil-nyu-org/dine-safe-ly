5ab976d6edac096dea12f335ada02aff3af371b9
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2020-11-10 21:12:05 -0500

    made env working

--- dinesafelysite/settings.py
+++ dinesafelysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +88,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
-        ".UserAttributeSimilarityValidator",
+                ".UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
@@ -120,26 +123,18 @@ STATICFILES_DIRS = [
 ]
 
 YELP_BUSINESS_API = "https://api.yelp.com/v3/businesses/"
-YELP_TOKEN_1 = (
-    "JaekzvTTKsWGtQ96HUiwAXOUwRt6Ndbqzch4zc2XFnOEBxwTmwr"
-    "-esm1uWo2QFvFJtXS8nY2dXx51cfAnMqVHpHRcp8N7QtP7LNVCcoxJWV_9NJrmZWSMiq"
-    "-R_mEX3Yx "
-)
-YELP_ACCESS_TOKEN2 = (
-    "A_V_V4rxelsvDsI2uFW1kT2mP2lUjd75GTEEsEcLnnvVOK5ssemrbw"
-    "-R49czpANtS2ZtAeCl6FaapQrp1_30cRt9YKao3pFL1I6304sAtwKwKJk"
-    "F1JBgF88FZl1_X3Yx "
-)
 
-YELP_ACCESS_TOKE = (
-    "w5fGYpYDI6NYJOBI47KjmEJcROpCxq1VK841olTs0tSGOeGBNDuIIj8zF"
-    "-C_MJFtAbrzfm7YF7bo72TxpOmrrn-zYnQ8xHBh_E4WEO39Z7IdKwbzCkBkCy0fjB6CX3Yx "
-)
+load_dotenv()
 
-YELP_TOKEN_CHUANQI = (
-    "xZS5Fvlspgk0IPac1_PTGHXShab3SErcTgweJcsDH77lXTDNbyaCFuH7c393oK41qXr0s_AnHb8fQ_"
-    "PgsTUsFxhQqwuRqtCPiZu51OWevYAql7syUQuxc3AQC_-lX3Yx "
-)
+load_dotenv(verbose=True)
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+YELP_TOKEN_1 = os.getenv("YELP_TOKEN_1")
+YELP_ACCESS_TOKEN2 = os.getenv("YELP_ACCESS_TOKEN2")
+YELP_ACCESS_TOKE = os.getenv("YELP_ACCESS_TOKE")
+YELP_TOKEN_CHUANQI = os.getenv("YELP_TOKEN_CHUANQI")
 
 # Yelp categories
 YELP_CATEGORY_API = "https://api.yelp.com/v3/categories"
--- manage.py
+++ manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinesafelysite.settings")
+    load_dotenv()
+
+    load_dotenv(verbose=True)
+
+    env_path = Path('.') / 'dinesafelysite' / '.env'
+    load_dotenv(dotenv_path=env_path)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
