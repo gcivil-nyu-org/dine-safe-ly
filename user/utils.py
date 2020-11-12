1cfa33aea92ce51dae15bb3842fad51255216123
ronakf11 <rf1999@nyu.edu>
2020-11-12 23:23:26 +0530

    Updated reset password email template

--- user/utils.py
+++ user/utils.py
@@ -3,6 +3,8 @@ from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
+from django import template
 
 import logging
 
@@ -13,14 +15,16 @@ def send_reset_password_email(request, email):
     user = User.objects.get(email=email)
     host_name = request.get_host()
     base_url = "http://" + host_name + "/user/reset_password/"
-    url = (
-        base_url
-        + urlsafe_base64_encode(force_bytes(user.pk))
-        + "/"
-        + PasswordResetTokenGenerator().make_token(user)
-    )
+    c = {
+        "base_url": base_url,
+        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': PasswordResetTokenGenerator().make_token(user),
+    }
+    htmltemp = template.loader.get_template('reset_password_template.html')
+    html_content = htmltemp.render(c)
     email_subject = "Reset Your Dine-safe-ly Password!"
-    message = url
+    # message = "Hello"
     logger.info("Send email to: %s", user.email)
-    email = EmailMessage(email_subject, message, to=[user.email])
+    email = EmailMultiAlternatives(email_subject, to=[user.email])
+    email.attach_alternative(html_content, "text/html")
     return email.send()
