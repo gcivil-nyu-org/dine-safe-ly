ed8b355fab2e3f8e07c671cb2a6633909dfc4f17
ronakf11 <rf1999@nyu.edu>
2020-11-24 00:45:33 +0530

    updated chatbot UI

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def chatbot(request):
     return render(
-        request=request, template_name="chat2.html"
+        request=request, template_name="chat.html"
     )
