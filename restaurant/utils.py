2ffac351ce58c240c6987c4f955ecdebd96dee9e
vinayaknatarajan1 <nvinayak777@gmail.com>
2020-11-23 21:20:25 -0500

    modified sort functionality

--- restaurant/utils.py
+++ restaurant/utils.py
@@ -253,13 +253,13 @@ def get_filtered_restaurants(
     value = None
     if sort_option:
         if sort_option == "ratedhigh":
-            value = "rating"
+            value = "-yelp_detail__rating"
         elif sort_option == "ratedlow":
-            value = "-rating"
+            value = "yelp_detail__rating"
         elif sort_option == "pricehigh":
-            value = "price"
+            value = "-yelp_detail__price"
         elif sort_option == "pricelow":
-            value = "-price"
+            value = "yelp_detail__price"
     if favorite_filter:
         if user.is_authenticated:
             if value:
@@ -268,8 +268,8 @@ def get_filtered_restaurants(
                     filtered_restaurants.filter(
                         business_id__in=YelpRestaurantDetails.objects.filter(
                             **filters
-                        ).order_by(value)
-                    )
+                        )#.order_by(value)
+                    ).order_by(value)
                     .distinct()
                     .filter(**keyword_filter)[offset : offset + int(limit)]
                 )
@@ -289,8 +289,8 @@ def get_filtered_restaurants(
             Restaurant.objects.filter(
                 business_id__in=YelpRestaurantDetails.objects.filter(
                     **filters
-                ).order_by(value)
-            )
+                )
+            ).order_by(value)
             .distinct()
             .filter(**keyword_filter)[offset : offset + int(limit)]
         )
