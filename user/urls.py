741622ed6874ea219a1f0933f7a4a9c8010ee6c5
chenwei713 <chenwei713@foxmail.com>
2020-11-23 20:54:26 -0500

    Modified the update password using Ajax

--- user/urls.py
+++ user/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     ),
     path("forget_password", views.forget_password, name="forget_password"),
     path("account_details", views.account_details, name="account_details"),
+    path("update_password", views.update_password, name="update_password")
 ]
