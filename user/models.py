884de5a78b451b83dd10e4c6cf890f1188b8c1cb
vinayaknatarajan1 <nvinayak777@gmail.com>
2020-11-23 21:20:00 -0500

    Updated model to add preferences

--- user/models.py
+++ user/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from restaurant.models import Restaurant
+from restaurant.models import Restaurant, Categories
 
 
 class DineSafelyUser(AbstractUser):
     favorite_restaurants = models.ManyToManyField(Restaurant, blank=True)
+    preferences = models.ManyToManyField(Categories, blank=True)
