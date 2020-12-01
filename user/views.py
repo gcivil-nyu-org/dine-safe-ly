6abaa61b560be457509c4e2a51ff90230a27131f
chenwei713 <chenwei713@foxmail.com>
2020-12-01 16:49:25 -0500

    black & flake8

--- user/views.py
+++ user/views.py
@@ -10,7 +10,7 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.auth import get_user_model
 from django.utils.encoding import force_text
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .utils import send_reset_password_email
 from .forms import UserCreationForm, ResetPasswordForm, UpdatePasswordForm, GetEmailForm
 
@@ -166,9 +166,7 @@ def update_password(request):
         for field in form:
             for error in field.errors:
                 error_list.append(error)
-        context = {
-            'status': '400', 'errors': error_list
-        }
-        response = HttpResponse(json.dumps(context), content_type='application/json')
+        context = {"status": "400", "errors": error_list}
+        response = HttpResponse(json.dumps(context), content_type="application/json")
         response.status_code = 400
         return response
